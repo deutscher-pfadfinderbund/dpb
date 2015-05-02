aeb077db729e3a23c7ae04e8a0d67c3e6fb0a938
Christian Meter <cmeter@googlemail.com>
2015-05-02 14:25:47 +0200

    Show filename in admin

--- archive/models.py
+++ archive/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
     signature = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
