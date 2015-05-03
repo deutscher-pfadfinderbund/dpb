ef55e4fbb54ee656e7b02d47a1286d8c9a004779
Christian Meter <cmeter@googlemail.com>
2015-05-03 16:34:56 +0200

    Revert

--- dpb/settings.py
+++ dpb/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
@@ -96,7 +96,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Configure Templates
 TEMPLATES = [
