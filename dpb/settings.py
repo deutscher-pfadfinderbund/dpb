e1ed745249c878eea5de38956a9f586b7ccffba2
n2o <cmeter@googlemail.com>
2015-05-03 17:13:53 +0200

    Change static root

--- dpb/settings.py
+++ dpb/settings.py
@@ -86,9 +86,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dpbstatic/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/var/www/dpbstatic/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
