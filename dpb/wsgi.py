7bd1381d503f6ce5cd15e6a0bf1273ed413df6ed
n2o <cmeter@googlemail.com>
2015-05-03 15:57:51 +0200

    Update settings

--- dpb/wsgi.py
+++ dpb/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.append('/usr/share/django/dpb')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dpb.settings")
 
 from django.core.wsgi import get_wsgi_application
