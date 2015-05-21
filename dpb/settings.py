09c240397f032364107dcde14ecaa1e55d303f04
Christian Meter <cmeter@googlemail.com>
2015-05-21 11:22:45 +0200

    Update settings

--- dpb/settings.py
+++ dpb/settings.py
@@ -90,8 +90,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/var/www/dpbstatic/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = "/var/www/dpbstatic/"
 STATIC_ROOT = "static/"
 
 # Define Paths for Pipeline
