1a1806591f95f949cb7554f78c888fa150d37f71
Christian Meter <cmeter@googlemail.com>
2015-06-01 21:05:34 +0200

    Add comments

--- dpb/settings.py
+++ dpb/settings.py
@@ -166,14 +166,7 @@ PIPELINE_JS = {
 ### END Pipeline ###
 
 # Configure TinyMCE
-TINYMCE_DEFAULT_CONFIG = {
-    'plugins': "table, spellchecker, paste, searchreplace",
-    'theme': "advanced",
-    'cleanup_on_startup': True,
-    'custom_undo_redo_levels': 10,
-}
-#TINYMCE_SPELLCHECKER = True
-#TINYMCE_COMPRESSOR = True
+# Find the TinyMCE configuration in /templates/admin/base_site.html
 
 
 # Configure Templates
