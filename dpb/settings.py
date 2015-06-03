68679ea05ddb23635c2325ba29a6887aa6aaa07e
Christian Meter <cmeter@googlemail.com>
2015-06-04 00:50:53 +0200

    Change img path for TinyMCE

--- dpb/settings.py
+++ dpb/settings.py
@@ -179,6 +179,8 @@ TEMPLATES = [{
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.media',
+                'django.core.context_processors.static',
             ],
         },
     },
