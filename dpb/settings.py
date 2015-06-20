333e04e4b63603b5805e9a16fd92076a19b3631b
Christian Meter <cmeter@googlemail.com>
2015-06-20 17:00:14 +0200

    Add admin to navbar

--- dpb/settings.py
+++ dpb/settings.py
@@ -100,7 +100,7 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = "/var/www/dpbstatic/"
-STATIC_ROOT = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Define Paths for Pipeline
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
