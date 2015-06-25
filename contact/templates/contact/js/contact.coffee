fffa699b61cf3d00ae04001416d390e365ad7dde
Christian Meter <cmeter@googlemail.com>
2015-06-25 17:42:32 +0200

    Move templates, add js file for contact

---
+++ contact/templates/contact/js/contact.coffee
@@ -0,0 +1,5 @@
+$ ->
+  console.log "Foo"
+
+  $("submit").click ->
+    console.log "Click"
--- dpb/settings.py
+++ dpb/settings.py
@@ -115,6 +115,7 @@ STATICFILES_FINDERS = (
 STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'media'),
     os.path.join(BASE_DIR, 'templates/dpb'),
+    os.path.join(BASE_DIR, 'contact/templates/contact'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
