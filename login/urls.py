5f11bb7bfa3fb4fe59284854351017bf9319f75b
Christian Meter <cmeter@googlemail.com>
2015-07-17 11:26:50 +0200

    Add correct urls for login

--- login/urls.py
+++ login/urls.py
@@ -4,8 +4,8 @@ from login.views import *
 
 urlpatterns = [
     url(r'^$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', logout_page),
-    url(r'^login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
-    url(r'^register/$', register),
+    url(r'^logout/$', logout_page, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register, name='register'),
     url(r'^register/success/$', register_success),
 ]
