2487dae47073fdc921954ac468aa31d65c00b123
Christian Meter <cmeter@googlemail.com>
2015-07-17 11:28:18 +0200

    Remove unused imports

--- login/forms.py
+++ login/forms.py
@@ -1,10 +1,8 @@
-import re
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 class RegistrationForm(forms.Form):
-
     username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
     email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
