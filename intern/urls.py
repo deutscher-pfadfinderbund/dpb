b017eadf3b797ffd220ceb3ce672bcbc6ce37b86
Christian Meter <cmeter@googlemail.com>
2015-07-26 00:26:16 +0200

    Rename documents

--- intern/urls.py
+++ intern/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'documents/$', views.documents, name='documents'),
+    url(r'dokumente/$', views.documents, name='documents'),
 ]
