9629097991c02610a85c9be1c262b4db025b0332
Christian Meter <cmeter@googlemail.com>
2015-08-02 00:11:55 +0200

    Define Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'archive', 'public', 'created')
+    list_filter = ['title']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,16 @@ from datetime import datetime
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField('Name', max_length=50, blank=False)
+
+
 class Post(models.Model):
     title = models.CharField('Titel', max_length=50, blank=False)
     content = models.TextField('Inhalt', max_length=1024, blank=False)
     archive = models.BooleanField('Archiviert?', default=False)
     public = models.BooleanField('Öffentlich?', default=True)
     created = models.DateTimeField('Erstellt am', default=datetime.now)
+    categories = models.ManyToManyField(Category)
 
 
-class Category(models.Model):
-    name = models.CharField('Name', max_length=50, blank=False)
