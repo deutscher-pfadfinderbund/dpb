7aa28ce10f914982eec24bd5cd56a05d99bfe0a1
Christian Meter <cmeter@googlemail.com>
2015-09-10 18:14:28 +0200

    Write small overview of dates

--- intern/urls.py
+++ intern/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'dokumente/$', views.documents, name='documents'),
+    url(r'termine/$', views.dates, name='dates'),
 ]
--- intern/views.py
+++ intern/views.py
@@ -2,8 +2,8 @@
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
-from filer.models import File
-from filer.models import Folder
+from filer.models import File, Folder
+from .models import Date
 
 
 @login_required
@@ -11,3 +11,8 @@ def documents(request):
     files = File.objects.all().order_by("-modified_at")
     folders = Folder.objects.all()
     return render(request, 'intern/documents.html', {'files': files, 'folders': folders})
+
+@login_required
+def dates(request):
+    dates = Date.objects.all().order_by("-date")
+    return render(request, 'intern/dates.html', {'dates': dates})
