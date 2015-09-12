8dff49600a000dde0b22aa2755bbeda6aa27a4c1
Christian Meter <cmeter@googlemail.com>
2015-09-12 20:36:26 +0200

    Overwrite Field with None if POI was not found in OSM

--- intern/models.py
+++ intern/models.py
@@ -13,17 +13,22 @@ class Date(models.Model):
     location = models.CharField('Ort', max_length=128, blank=True)
     latitude = models.FloatField('Breitengrad', max_length=128, blank=True, null=True)
     longitude = models.FloatField('Längengrad', max_length=128, blank=True, null=True)
-    display_name = models.CharField('Berechneter Standort', max_length=128, blank=True)
+    display_name = models.CharField('Berechneter Standort', max_length=128, blank=True, null=True)
     description = models.TextField('Beschreibung', blank=True)
     host = models.CharField('Ausrichter', max_length=128, blank=True)
     attachment = FilerFileField(verbose_name='Anhang', null=True, blank=True, related_name="date_attachment")
     created = models.DateTimeField('Erstellt am', default=datetime.now)
 
     def clean(self):
-        data = requests.get("http://nominatim.openstreetmap.org/search?q=" + str(self.location) + "&format=json&polygon=1&addressdetails=1").json()[0]
-        self.latitude = data["lat"]
-        self.longitude = data["lon"]
-        self.display_name = data["display_name"]
+        try:
+            data = requests.get("http://nominatim.openstreetmap.org/search?q=" + str(self.location) + "&format=json&polygon=1&addressdetails=1").json()[0]
+            self.latitude = data["lat"]
+            self.longitude = data["lon"]
+            self.display_name = data["display_name"]
+        except:
+            self.latitude = None
+            self.longitude = None
+            self.display_name = None
 
     def __str__(self):
         return self.title
