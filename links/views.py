9f75ffcd96ff85412b9f1e4967c584fbd79737c9
Christian Meter <cmeter@googlemail.com>
2015-09-25 13:41:02 +0200

    Reverse objects

--- links/views.py
+++ links/views.py
@@ -4,6 +4,6 @@ from .models import Link, LinkCategory
 
 
 def links(request):
-    links = Link.objects.all().order_by("-title")
-    cats = LinkCategory.objects.all().order_by("-name")
+    links = Link.objects.all().order_by("title")
+    cats = LinkCategory.objects.all().order_by("name")
     return render(request, 'links.html', {'links': links, 'cats': cats})
