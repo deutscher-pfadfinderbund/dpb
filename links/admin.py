4cb3067eb768dad6d92b6511b952fe59e6fe708b
Christian Meter <cmeter@googlemail.com>
2015-09-25 12:54:33 +0200

    Configure Categories

--- links/admin.py
+++ links/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Link
+from .models import Link, LinkCategory
 
 
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'website')
-    search_fields = ['title, website']
+    list_display = ('title', 'category', 'website')
+    search_fields = ['title']
 
 
 admin.site.register(Link, LinkAdmin)
+admin.site.register(LinkCategory)
