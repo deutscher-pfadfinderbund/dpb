a005f28e3b1463fe28e6e287d5e62e418da07d48
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-21 18:20:49 +0100

    Use bootstrap form for login

--- login/urls.py
+++ login/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', authviews.login),
     url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^login/$', authviews.login, name='login'),  # If user is not logged in, it will redirect to login page
-    url(r'^register/$', views.register, name='register'),
-    url(r'^register/success/$', views.register_success, name='register_success'),
+    # url(r'^register/$', views.register, name='register'),
+    # url(r'^register/success/$', views.register_success, name='register_success'),
 ]
