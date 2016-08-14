9f5c37c9c7d907deea6cdcd7ff503b53b71095d1
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-08-14 13:46:03 +0200

    Tidy up

--- archive/urls.py
+++ archive/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^$', views.index, name='index'),
-    url(r'suchen/$', views.search, name='search'),
+    url(r'katalog/$', views.search, name='search'),
     url(r'einschicken/$', views.send_in, name='send_in'),
 ]
