14d259fe482deb3f433f8f8cd36fbb9d76990a00
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-10-22 15:15:19 +0200

    Show evening_programs

--- evening_program/views.py
+++ evening_program/views.py
@@ -2,7 +2,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Program
+
 
 @login_required
 def evening_program(request):
-    return render(request, 'evening_program/index.html')
+    programs = Program.objects.all().order_by("title")
+    return render(request, "evening_program/index.html", {"programs": programs})
