ebdb9dafbcc26952154c8f68727d8f6a72250fb8
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-10-22 17:15:42 +0200

    Include evening_program into navigation

--- evening_program/models.py
+++ evening_program/models.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 
 class Program(models.Model):
-    title = models.CharField("Titel", max_length=256, blank=False)
-    target = models.CharField("Wie alt sind meine Mädchen / Jungen?", max_length=128, blank=False)
+    title = models.CharField("Titel *", max_length=256, blank=False)
+    target = models.CharField("Wie alt sind meine Mädchen / Jungen? *", max_length=128, blank=False)
     preparation = models.TextField("Was habe ich vorbereitet?", max_length=4096, blank=True)
-    execution = models.TextField("Was haben wir gemacht?", max_length=4096, blank=False)
+    execution = models.TextField("Was haben wir gemacht? *", max_length=4096, blank=False)
     greatness = models.TextField("Was war daran so toll?", max_length=4096, blank=True)
     contact_name = models.CharField("Mein Name", max_length=128, blank=True)
     contact_group = models.CharField("Meine Gruppe", max_length=128, blank=True)
