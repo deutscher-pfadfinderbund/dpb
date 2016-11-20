cf3a4ae27eadeae556222603666a49a490bf0506
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-11-20 21:42:50 +0100

    Remove metamodel

--- dpb/models.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-
-class ModifiedModel(models.Model):
-    created = models.DateTimeField("Erstellt am", default=datetime.now)
-    modified = models.DateTimeField("Zuletzt geändert", auto_now=True)
