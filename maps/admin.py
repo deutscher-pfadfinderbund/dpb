f1c299b42aa8c9a299765ab5f4ff7d776bd0b0b2
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-11-20 22:26:55 +0100

    Add admin, update OSM coordinates

--- maps/admin.py
+++ maps/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import GroupMaps
 
-# Register your models here.
+
+@admin.register(GroupMaps)
+class GroupMapsAdmin(admin.ModelAdmin):
+    list_display = ("group", "location1")
+    search_fields = ["group"]
+    readonly_fields = ["latitude1", "longitude1", "latitude2", "longitude2", "latitude3", "longitude3"]
+    fieldsets = [
+        ("Allgemein", {"fields": ["boysgirls", "group", "leader", "subgroups", "special", "other", "website", "public"]}),
+        ("Locations", {"fields": [
+            ("location1", "latitude1", "longitude1"),
+            ("location2", "latitude2", "longitude2"),
+            ("location3", "latitude3", "longitude3"),
+        ]}),
+        ("Erweitert", {"fields": ["created"], "classes": ["collapse"]}),
+    ]
--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,63 @@
+# -*- coding: utf-8 -*-
+from autoslug import AutoSlugField
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+import requests
+
+
+def query_latlong(location):
+    data = requests.get("https://nominatim.openstreetmap.org/search?q=" + str(location) + " Deutschland" + "&format=json&polygon=1&addressdetails=1").json()[0]
+    return data["lat"], data["lon"]
+
+
+def catch_query(location):
+    latitude = longitude = None
+    if location:
+        try:
+            latitude, longitude = query_latlong(location)
+        except IndexError:
+            latitude = longitude = None
+    return latitude, longitude
+
+
+class GroupMaps(models.Model):
+    boysgirls_choices = (
+        ("Mädels", "Mädels"),
+        ("Jungs", "Jungs"),
+        ("Gemischt", "Gemischt"),
+    )
+    boysgirls = models.CharField("Mädels / Jungs / gemischt", max_length=256, choices=boysgirls_choices, blank=False)
+    group = models.CharField("Gruppierung", max_length=256, blank=False)
+    leader = models.CharField("Unser/e Führer/in ist", max_length=256, blank=False)
+    subgroups = models.CharField("Untergeordnete Gruppen", max_length=1024, blank=True, null=True)
+    special = models.TextField("Das zeichnet uns aus", max_length=4096, blank=True)
+    other = models.TextField("Was ihr uns sonst noch mitteilen möchtet", max_length=4096, blank=True)
+
+    location1 = models.CharField("Standort 1", max_length=1024, blank=False)
+    latitude1 = models.FloatField("Breitengrad 1", max_length=4096, blank=True, null=True)
+    longitude1 = models.FloatField("Längengrad 1", max_length=4096, blank=True, null=True)
+    location2 = models.CharField("Standort 2", max_length=1024, blank=True)
+    latitude2 = models.FloatField("Breitengrad 2", max_length=4096, blank=True, null=True)
+    longitude2 = models.FloatField("Längengrad 2", max_length=4096, blank=True, null=True)
+    location3 = models.CharField("Standort 3", max_length=1024, blank=True)
+    latitude3 = models.FloatField("Breitengrad 3", max_length=4096, blank=True, null=True)
+    longitude3 = models.FloatField("Längengrad 3", max_length=4096, blank=True, null=True)
+
+    website = models.URLField("Webseite", blank=True)
+    slug = AutoSlugField(populate_from='group', unique=True)
+    public = models.BooleanField("Öffentlich?", default=False)
+    created = models.DateTimeField("Erstellt am", default=datetime.now)
+    modified = models.DateTimeField("Zuletzt geändert", auto_now=True)
+
+    def clean(self):
+        self.latitude1, self.longitude1 = catch_query(self.location1)
+        self.latitude2, self.longitude2 = catch_query(self.location2)
+        self.latitude3, self.longitude3 = catch_query(self.location3)
+
+    def __str__(self):
+        return self.group
+
+    class Meta:
+        verbose_name = "Gruppenkarte"
+        verbose_name_plural = "Gruppenkarten"
