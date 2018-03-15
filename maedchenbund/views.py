3c60fd42d7ce84b0f90d80d6e04b46c8affb5ff5
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-03-15 09:38:55 +0100

    Add required login to maedchenbund

--- maedchenbund/views.py
+++ maedchenbund/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Document
 
 
+@login_required
 def documents(request):
     docs = Document.objects.all().order_by("title")
     return render(request, 'home.html', {'documents': docs})
