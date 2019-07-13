a5dae22e5602682ac6259fe82b40fe8cf79cea7a
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-07-13 10:35:16 +0200

    Update postgres version and change password

--- start_database.sh
+++ start_database.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name dpbdb -v /home/n2o/Repos/dpb/db/entrypoint:/docker-entrypoint-initdb.d --rm -e POSTGRES_PASSWORD=aFNsEQQC9Gk8NP7urjt4HBegMudJ1zuK -e POSTGRES_ROOT_PASSWORD=aFNsEQQC9Gk8NP7urjt4HBegMudJ1zuK -e POSTGRES_USER=dpb -e POSTGRES_DB=dpb -p 5432:5432 postgres:10-alpine
+docker run --name dpbdb -v /home/n2o/Repos/dpb/db/entrypoint:/docker-entrypoint-initdb.d --rm -e POSTGRES_PASSWORD=razupaltuff -e POSTGRES_ROOT_PASSWORD=razupaltuff -e POSTGRES_USER=dpb -e POSTGRES_DB=dpb -p 5432:5432 postgres:11-alpine
