23747002e9b32b5f46db0c69c663dad427481642
Christian Meter <cmeter@googlemail.com>
2019-10-28 17:39:43 +0100

    Use `pwd` to find path to database data

--- start_database.sh
+++ start_database.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name dpbdb -v /home/n2o/Repos/dpb/db/entrypoint:/docker-entrypoint-initdb.d --rm -e POSTGRES_PASSWORD=razupaltuff -e POSTGRES_ROOT_PASSWORD=razupaltuff -e POSTGRES_USER=dpb -e POSTGRES_DB=dpb -p 5432:5432 postgres:11-alpine
+docker run --name dpbdb -v `pwd`/db/entrypoint:/docker-entrypoint-initdb.d --rm -e POSTGRES_PASSWORD=razupaltuff -e POSTGRES_ROOT_PASSWORD=razupaltuff -e POSTGRES_USER=dpb -e POSTGRES_DB=dpb -p 5432:5432 postgres:11-alpine
