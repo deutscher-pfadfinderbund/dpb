9385010a8b8a182d00b6c6b20074a1694d096a06
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-01-24 17:28:04 +0100

    Add notes field

--- address_book/models.py
+++ address_book/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 
 class Person(models.Model):
-    salutation = models.CharField("Anrede", max_length=50, null=False, blank=False)
+    salutation = models.CharField("Anrede", max_length=50)
     title = models.CharField("Titel", max_length=50, null=True)
-    first_name = models.CharField('Vorname', max_length=50, null=False, blank=False)
-    last_name = models.CharField('Nachname', max_length=50, null=False, blank=False)
-    nickname = models.CharField('Fahrtenname', max_length=50, blank=False, null=True)
+    first_name = models.CharField('Vorname', max_length=50)
+    last_name = models.CharField('Nachname', max_length=50)
+    nickname = models.CharField('Fahrtenname', max_length=50, null=True)
     birthdate = models.DateField("Geburtstag", null=True)
     date_of_death = models.DateField("Todestag", null=True)
     email = models.EmailField("Email", null=True)
+
+    notes = models.TextField("Anmerkung", null=True, blank=True)
 
     def common_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -29,15 +31,15 @@ class Person(models.Model):
         verbose_name_plural = 'Personen'
 
 
-class Adress(models.Model):
-    person = models.ForeignKey(Person, null=False)
+class Address(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
     street = models.CharField("Straßenname", max_length=100)
     zip = models.CharField("PLZ", max_length=10)
     town = models.CharField("Stadt", max_length=50)
 
 
 class PhoneNumber(models.Model):
-    person = models.ForeignKey(Person, null=False)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
     label = models.CharField("Bezeichner", max_length=100)
     phone_regex = RegexValidator(regex=r'^\d+$')
     number = models.CharField(validators=[phone_regex], max_length=20)
