df52564ee9bf592af9fd73fd79df63938a5aece2
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-11-22 22:09:09 +0100

    Update leaflet and remove commited assets

--- dpb/settings.py
+++ dpb/settings.py
@@ -111,6 +111,7 @@ STATICFILES_FINDERS = (
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'templates/dpb'),
     os.path.join(BASE_DIR, 'contact/templates/contact'),
+    ("node_modules", os.path.join(BASE_DIR, 'node_modules')),
     ("bootstrap", os.path.join(BASE_DIR, 'node_modules/bootstrap/dist/js')),
     ("jquery", os.path.join(BASE_DIR, 'node_modules/jquery/dist')),
     ("styles", os.path.join(BASE_DIR, 'styles')),
