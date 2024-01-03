1dfdf79d4bbaaf1b94862669fa7710510c1b054a
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2024-01-03 03:27:44 +0100

    Get rid of deprecated `ugettext_lazy`

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<url>.*)$', views.page, name='pages'),
+    re_path(r'^(?P<url>.*)$', views.page, name='pages'),
 ]
