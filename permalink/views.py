cec9ebde8893d5cad4fa79d03072f7bf9310a3ce
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2024-01-16 15:37:08 +0100

    Don't permanently redirect permalinks

--- permalink/views.py
+++ permalink/views.py
@@ -4,4 +4,4 @@ from .models import Permalink
 
 
 def redirectPermanent(request, slug):
-    return redirect(get_object_or_404(Permalink, short=slug).redirect_url, permanent=True)
+    return redirect(get_object_or_404(Permalink, short=slug).redirect_url)
