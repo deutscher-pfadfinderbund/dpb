1d2b17860017d50076e0cf20930a63f5be97c020
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2024-09-11 13:55:14 +0200

    Remove unreachable page redirect

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import Http404, HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
@@ -35,38 +36,27 @@ def page(request, url):
     if not url.startswith('/'):
         url = '/' + url
     site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(Page, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith('/') and settings.APPEND_SLASH:
-            url += '/'
-            f = get_object_or_404(Page, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect('%s/' % request.path)
-        else:
-            raise
-    return render_page(request, f)
+    return render_page(request, get_object_or_404(Page, url=url, sites=site_id))
 
 
 @csrf_protect
-def render_page(request, f):
+def render_page(request, page: Page):
     """
     Internal interface to the flat page view.
     """
     # If registration is required for accessing this page, and the user isn't
     # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
+    if page.registration_required and not request.user.is_authenticated:
         return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    if page.template_name:
+        template = loader.select_template((page.template_name, DEFAULT_TEMPLATE))
     else:
         template = loader.get_template(DEFAULT_TEMPLATE)
 
     # To avoid having to always use the "|safe" filter in flatpage templates,
     # mark the title and content as already safe (since they are raw HTML
     # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
+    page.title = mark_safe(page.title)
+    page.content = mark_safe(page.content)
 
-    response = HttpResponse(template.render({'page': f}, request))
-    return response
+    return HttpResponse(template.render({'page': page}, request))
